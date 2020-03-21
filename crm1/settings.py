495c997a65d0b81f30cf5136fed7aaaecb6dd85f
ahmadfaizuddin17 <ahmadfaizuddin17@gmail.com>
2020-03-21 21:35:14 +0900

    debug → False

--- crm1/settings.py
+++ crm1/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 MEDIA_URL = "/images/"
