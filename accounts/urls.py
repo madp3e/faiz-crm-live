70fa636b33370c647ef46cc3a2f4a093888bdc96
ahmadfaizuddin17 <ahmadfaizuddin17@gmail.com>
2020-03-21 19:23:02 +0900

    First origin push

---
+++ accounts/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from django.contrib.auth import views as auth_view
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("products/", views.products, name="products"),
+    path("customer/<int:pk>/", views.customer, name="customer"),
+    path("create_order/<int:pk>/", views.create_order, name="create_order"),
+    path("update_order/<int:pk>/", views.update_order, name="update_order"),
+    path("delete_order/<int:pk>/", views.delete_order, name="delete_order"),
+    path("register/", views.registerPage, name="register"),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("user/", views.userPage, name="user-page"),
+    path("account/", views.accountSettings, name="account"),
+
+    path("reset_password/", auth_view.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="reset_password"),
+
+    path("reset_password_sent/", auth_view.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>", auth_view.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path("reset_password_complete/", auth_view.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+         name="password_reset_complete"),
+
+]
